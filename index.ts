b056144dd060ba629d98f90403a0122d18c7d2ed
gcgj123 <601140987@qq.com>
2018-12-20 23:56:43 +0800

    index

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-var a: number;
-var b: boolean;
-var c: string;
+// var a: number;
+// var b: boolean;
+// var c: string;
 
-a = 111;
-b = true;
-c = "hello world";
+// a = 111;
+// b = true;
+// c = "hello world";
+
+
+// let myArr: number[] = [1, 2, 3];
+
+let myArr: Array<boolean> = [false, false, true];
+
+// myArr = [];
+myArr.push(false)
+let a = myArr.pop();
+console.log(a);
+
+let my_name: string = "rails365";
+
+let other_name = my_name;
+console.log(other_name)
